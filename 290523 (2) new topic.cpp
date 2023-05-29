9da7e40bd6659e4789e5aaf3e2fe9ecdce64e374
Admin <shadow00713@yandex.ru>
2023-05-29 19:52:04 +0300

    Add project files.

---
+++ 290523 (2) new topic.cpp
@@ -0,0 +1,11 @@
+﻿// 290523 (2) new topic.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
